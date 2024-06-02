eff46a7f2646992a1f3fa6ac9e83927380b744ce
Mettralla <67493167+Mettralla@users.noreply.github.com>
2024-06-02 20:34:31 -0300

    Added additional features in exchange rate between dates

--- ejercicio3/converter.js
+++ ejercicio3/converter.js
@@ -37,6 +37,36 @@ class CurrencyConverter {
             return null;
         }
     }
+
+    // FUNCIONALIDADES ADICIONALES
+    async getExchangeRate(date, fromCurrency, toCurrency) {
+        try {
+            const response = await fetch(`${this.apiUrl}/${date}?from=${fromCurrency.code}&to=${toCurrency.code}`);
+            const rateData = await response.json();
+            return Number(rateData.rates[toCurrency.code]);
+        } catch (error) {
+            console.error(error);
+            return null;
+        }
+    }
+    
+    async getDifferenceBetweenDates(fromCurrency, toCurrency) {
+        const today = new Date();
+        const yesterday = new Date();
+        yesterday.setDate(today.getDate() - 1);
+
+        const todayStr = today.toISOString().split('T')[0];
+        const yesterdayStr = yesterday.toISOString().split('T')[0];
+
+        const rateToday = await this.getExchangeRate(todayStr, fromCurrency, toCurrency);
+        const rateYesterday = await this.getExchangeRate(yesterdayStr, fromCurrency, toCurrency);
+
+        if (rateToday === null || rateYesterday === null) {
+            return null;
+        }
+
+        return rateToday - rateYesterday;
+    }
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
@@ -72,6 +102,20 @@ document.addEventListener("DOMContentLoaded", async () => {
             resultDiv.textContent = `${amount} ${
                 fromCurrency.code
             } son ${convertedAmount.toFixed(2)} ${toCurrency.code}`;
+
+            // **TASA DE CAMBIO - MUESTRA UNA LINEA LUEGO DE LA CONVERSION**
+            const difference = await converter.getDifferenceBetweenDates(fromCurrency, toCurrency);
+            if (difference !== null) {
+                const differenceDiv = document.createElement("p");
+                differenceDiv.textContent = `La diferencia en la tasa de cambio entre hoy y ayer es de ${difference.toFixed(4)}`;
+                resultDiv.appendChild(differenceDiv);
+            } else {
+                const errorDiv = document.createElement("p");
+                errorDiv.textContent = "Error al obtener la diferencia en tasas de cambio.";
+                resultDiv.appendChild(errorDiv);
+            }
+            // **TASA DE CAMBIO**
+
         } else {
             resultDiv.textContent = "Error al realizar la conversión.";
         }
